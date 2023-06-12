815196e898689d2267fc6b0ebe28c4771b93a975
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-06-12 09:36:09 +0100

    Implemented light and dark theme Installed and started using Phosphor-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,39 @@
-import hamburger from "../icons/hamburger.svg"
-import moon from "../icons/moon.svg"
+import { useState} from "react";
+import React from 'react';
+import {List, Sun, Moon } from "@phosphor-icons/react"
+
 
 function Navbar() {
+     
+    // This function handles the setting and updating of the page's icon
+    const moonIcon = <Moon weight="fill" alt="Moon icon"className="sm:text-[45px] text-[30px]" />;
+    const sunIcon = <Sun weight="fill" alt="Sun icon" color="white" className="sm:text-[45px] text-[30px]" />;
+
+
+    const [icon, setIcon] = useState(moonIcon)
+    const [theme, setTheme] = useState(true)
+
+    const updateIcon = ()=>{
+        if(theme){
+            setTheme(false)
+            setIcon(sunIcon)
+            document.body.classList.add("dark")
+        }
+        else if(!theme){
+            setTheme(true)
+            setIcon(moonIcon)
+            document.body.classList.remove("dark")
+        }
+    }
+
     return(
-    <div className="w-[full] h-[70px] dark:bg:black flex justify-center">
+    <div className="w-[full] h-[70px] dark:bg-black flex justify-center">
         <div className="w-[85%] h-[100%] flex justify-between items-end">
-            <span className="sm:w-[45px] sm:h-[45px] w-[30px] h-[30px] active:scale-[1.05]"><img src={moon} height="100" width="100" alt="moon icon" /></span>
-            <span className="sm:w-[45px] sm:h-[45px] w-[30px] h-[30px] active:scale-[1.05]"><img src={hamburger} height="100" width="100" alt="hamburger icon" /></span>
-        </div>
+            <span onClick={updateIcon} className=" active:scale-[1.05]">{icon}</span>
+            <span className=" active:scale-[1.05] ">
+            <List weight="fill" className="dark:text-white sm:text-[45px] text-[30px]" alt="Hamburger icon"/>
+            </span> 
+            </div>
     </div>
     )
 }
--- src/components/QuotesBar.js
+++ src/components/QuotesBar.js
@@ -1,16 +1,35 @@
+import axios from "axios";
+import { useState} from "react";
+
 function QuotesBar(){
+    // const limit = 1;
+    const [fact, setFact] = useState("Click to spurn facts that will give your cracky goosebumpsss")
+
+    const getFact = ()=>{
+        axios({
+            method: "get",
+            url: "https://api.api-ninjas.com/v1/facts?limit=1",
+            headers: {'x-api-key': "y0/1+EwtLLUc5ewzOZo+Ug==TDHzUPKV4gBonwrl"},
+            contentType: "application/json"
+
+        })
+        .then((res)=>{setFact(res.data[0].fact)})
+        .catch((err)=>{console.log(err)})
+
+    }
+
     return(
-        <div className="w-[100%] h-[calc(100vh-70px)] flex justify-center items-center px-[40px]">
+        <div className="w-[100%] h-[calc(100vh-70px)] flex justify-center items-center px-[40px] dark:bg-black">
             <div className="w-[750px] h-fit ">
-                <p className="sm:text-[30px] font-font1 text-[20px] font-bold ">In the year 1998, John F Kennedy, The President of The United States Of America Was Assasinated on the Great March Day</p>
+                <p className="sm:text-[30px] font-font1 text-[20px] font-bold dark:text-white select-none">{fact}</p>
 
                 <div className="sm:w-[300px] w-[180px] h-fit flex justify-center items-center mx-auto mt-[50px]">
-                    <div className="w-[200px] h-[3px] bg-black"></div>
-                    <div className="w-[10px] h-[10px] bg-black rounded-full shrink-0 m-[5px]"></div>
-                    <div className="w-[200px] h-[3px] bg-black"></div>
+                    <div className="w-[200px] h-[3px] bg-black dark:bg-white"></div>
+                    <div className="w-[10px] h-[10px] bg-black rounded-full shrink-0 m-[5px] dark:bg-white"></div>
+                    <div className="w-[200px] h-[3px] bg-black dark:bg-white"></div>
                 </div>
 
-                <div className="w-[100%] h-fit mt-[50px] mx-auto flex justify-center items-center active:scale-[1.03]">
+                <div onClick={getFact} className="w-[100%] h-fit mt-[50px] mx-auto flex justify-center items-center active:scale-[1.03]">
                     <div className="w-[150px] h-[40px] sm:w-[250px] sm:h-[70px] relative">
                     <div className="w-[100%] h-[100%] sm:text-[25px] bg-[#009688] text-white font-font1 font-bold flex justify-center items-center select-none">See New Fact
                     </div>
