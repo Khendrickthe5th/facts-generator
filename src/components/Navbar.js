bad1564e076f839d10f21a3c1211212223e4c836
Ozoani Chidubem <flyinglego153971@gmail.com>
2025-04-15 13:24:07 +0100

    Updated the api ninha header params

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,13 +40,13 @@ function Navbar() {
             <span onClick={updateHam} className=" active:scale-[1.05] ">{HamIcon}</span> 
             </div>
 
-            <div ref={setDisplay} className="w-[50%] h-[100%] sm:w-[35%] absolute right-0 hidden bg-white dark:bg-black z-[20] shadow-md shadow-black">
+            <div ref={setDisplay} className="w-[90%] h-[100%] sm:w-[65%] absolute right-0 hidden bg-white dark:bg-black z-[20] shadow-md shadow-black">
                 <div className="w-[85%] sm:w-[80%] h-[100%]">
                     <span onClick={()=>{setDisplay.current.classList.add("hidden")}} className="w-[100%] h-[70px] flex items-end justify-end"><XSquare weight="bold" className="dark:text-white sm:text-[43px] text-[28px]"/></span>
 
                     <span onClick={()=>{alert("Feature coming soon!")}} className="w-[100%] h-[70px] flex justify-center items-end font-font1 font-semibold text-[13px] sm:text-[21px] dark:text-white"><a href={void 0}>Random Quotes</a></span>
                     
-                    <span className="w-[100] h-[calc(100%-140px)] text-[10px] sm:text-[15px] font-font1 font-normal flex items-end justify-center pb-[20px] dark:text-white"><p>Made with 💖 by Ozoani Chidubem</p></span>
+                    <span className="w-[100] h-[calc(100%-140px)] text-[10px] sm:text-[15px] font-font1 font-normal flex items-end justify-center pb-[20px] dark:text-white"><p>Made with 💖 by Khendrick</p></span>
                 </div>
             </div>
     </div>
